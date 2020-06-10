cccab0db85f7876d820b3e4fba0b80e4226e2db3
bilichenCOM <bilichenko.mykhailo@gmail.com>
2020-06-10 22:27:19 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainPageComponent,
+    FooterComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
